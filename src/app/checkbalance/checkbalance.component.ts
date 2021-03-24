3728fe43b2e4ec968c60d8eebf61e9caf2b44a6d
Jordan Chen <jordanchen98@gmail.com>
2021-03-24 11:12:38 +0800

    Added Changes to Check Balance Component
    Added Changes to component for Check Balance, and added extra fields to unitholder to cater all api scenarios.

--- src/app/checkbalance/checkbalance.component.ts
+++ src/app/checkbalance/checkbalance.component.ts
@@ -6,6 +6,8 @@ import { signalrConnection } from 'src/app/_models/signalr';
 import { ServiceService } from 'src/app/_shared/service.service';
 import { finalize } from 'rxjs/operators';
 import { currentHolder } from 'src/app/_models/currentUnitHolder';
+import { fundDetails } from '../_models/fundDetails';
+import { minorDetails } from '../_models/minorDetails';
 
 @Component({
   selector: 'app-checkbalance',
@@ -13,7 +15,9 @@ import { currentHolder } from 'src/app/_models/currentUnitHolder';
   styleUrls: ['./checkbalance.component.css']
 })
 export class CheckbalanceComponent implements OnInit {
-  
+
+  mDetails : any = currentHolder.minordetail;
+  fDetails : any = currentHolder.funddetail;
   BTN_Cancel = "";
   BTN_MainMenu = "";
 
@@ -33,6 +37,8 @@ export class CheckbalanceComponent implements OnInit {
   CB3_Visible = false;
   CB4_Visible = false;
   CB5_Visible = false;
+
+  CBBijak_Visible = false;
 
   CB2_ErrorVisible = false;
 
@@ -84,11 +90,19 @@ export class CheckbalanceComponent implements OnInit {
   ngOnInit(): void {
 
     
-    // this.translate.use(selectLang.selectedLang);
-    this.translate.use('en');
+    this.translate.use(selectLang.selectedLang);
+    //this.translate.use('ms');
     
     this.noAhli = currentHolder.unitholderid;
     this.namaAhli = currentHolder.firstname;
+
+
+    if(currentHolder.totalminoraccount != "0"){
+      this.CBBijak_Visible = true;
+    }
+    else{
+      this.CBBijak_Visible = false;
+    }
   }
   
   CancelCheckBalance() {
@@ -118,11 +132,12 @@ export class CheckbalanceComponent implements OnInit {
     this.CB2_5 = currentHolder.identificationnumber;
     this.CB2_7 = currentHolder.unitholderid;
 
+    this.CB2_9 = currentHolder.grandtotaluhholdings;
+    this.CB2_10 = currentHolder.grandtotalunits;
+
     //this.CB2_9 = currentHolder.
     //this.CB2_10 = currentHolder.
   }
-
-
   
 
 }
