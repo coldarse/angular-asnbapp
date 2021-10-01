270117dd683217320469d6d354ef0fd340a9ce08
SAM WONG <haohsienwong@hotmail.com>
2021-10-01 11:28:02 +0800

    SAM Commit

--- src/app/subscriptioninvestment/subscriptioninvestment.component.ts
+++ src/app/subscriptioninvestment/subscriptioninvestment.component.ts
@@ -3310,7 +3310,7 @@ export class SubscriptioninvestmentComponent implements OnInit {
 
   isBefore4pm(){
     let today = new Date().getHours();
-    if (today < 16) {
+    if (today < 20) { //Temporary change to 20, 8pm
       return true;
     } else {
       return false;
